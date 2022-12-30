0c64d214f08e4a07fcd78484a7090a69017c3b16
Ryo Rifaldhi <ryonewbie@gmail.com>
2022-12-30 16:27:25 +0700

    MODUL3
    MODUL3

---
+++ MODUL3_RYO/Calculation.java
@@ -0,0 +1,78 @@
+package MODUL3;
+public class Calculation implements Runnable{
+    private double radius;
+    private double side;
+    private double area;
+    private final double phi = 3.14;
+
+    public void setSquare(double side) throws IllegalArgumentException {
+        if ( side < 1 ) {
+            throw new IllegalArgumentException("Must be greater than 1");
+        }
+        this.area = side * side;
+    }
+    public double getSquare() {
+        return this.area;
+    }
+
+    public void setCircle(double radius) throws IllegalArgumentException {
+        if ( radius < 1 ) {
+            throw new IllegalArgumentException("Must be greater than 1");
+        }
+        this.area = phi * radius * radius;
+    }
+    public double getCircle() {
+        return this.area;
+    }
+
+    public void setTrapezoid(double a, double b, double t) throws IllegalArgumentException {
+      if (a < 1 || b < 1 || t < 1) {
+        throw new IllegalArgumentException("Must be greater than 1");
+      }
+      this.area = ((a + b) * t) / 2;
+    }
+    
+    public double getTrapezoid() {
+        return this.area;
+      }
+      public double getRadius() {
+        return radius;
+      }
+    
+      public void setRadius(double radius) {
+        this.radius = radius;
+      }
+    
+      public double getSide() {
+        return side;
+      }
+    
+      public void setSide(double side) {
+        this.side = side;
+      }
+    
+      public double getArea() {
+        return area;
+      }
+    
+      public void setArea(double area) {
+        this.area = area;
+      }
+    
+      public double getPhi() {
+        return phi;
+      }
+
+      @Override
+      public void run() {
+        System.out.println("=====Program Will Start In=====");
+        for (int i = 3; i > 0; i--) {
+            try {
+                System.out.println("Starting with thread "+i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Error");
+            } 
+        }
+      }
+}
